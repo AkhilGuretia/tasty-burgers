af22a957f6b8b976fc9c8a212d1eb31045d9c0f4
Akhil Kumar <akhilguretia@gmail.com>
2024-08-08 23:23:30 +0530

    Foooter updates

--- src/pages/Homepage/Section7.jsx
+++ src/pages/Homepage/Section7.jsx
@@ -14,7 +14,7 @@ function Section7() {
               dolor primis libero tempus, blandit a cursus varius luctus neque
               magna
             </p>
-            <Link to="/" className="btn btn_red px-4 py-2 rounded-0">
+            <Link to="/" className="btn_red px-4 py-2 rounded-0">
               Call: 999-888-7777
             </Link>
           </Col>

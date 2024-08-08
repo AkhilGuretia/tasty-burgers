a0c5f7556ae91416241a6b14c431bfcdea5410a2
Akhil Kumar <akhilguretia@gmail.com>
2024-08-08 22:54:19 +0530

    Section 3, 4 and 5

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -2,6 +2,9 @@ import Layout from "../../components/Layouts/Layout";
 import Section1 from "./Section1";
 import "../../styles/HomeStyles.css";
 import Section2 from "./Section2";
+import Section3 from "./Section3";
+import Section4 from "./Section4";
+import Section5 from "./Section5";
 
 function Home() {
   return (
@@ -9,6 +12,9 @@ function Home() {
       <Layout>
         <Section1 />
         <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
       </Layout>
     </>
   );

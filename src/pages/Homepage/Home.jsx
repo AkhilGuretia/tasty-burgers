024f8218846da9735be96851459e8e2beaf57fae
Akhil Kumar <akhilguretia@gmail.com>
2024-08-08 23:02:02 +0530

    Section 6 and 7

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -5,6 +5,8 @@ import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
+import Section6 from "./Section6";
+import Section7 from "./Section7";
 
 function Home() {
   return (
@@ -15,6 +17,8 @@ function Home() {
         <Section3 />
         <Section4 />
         <Section5 />
+        <Section6 />
+        <Section7 />
       </Layout>
     </>
   );

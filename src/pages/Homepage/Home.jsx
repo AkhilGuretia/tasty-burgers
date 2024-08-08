27448c8709252c7683763fcf3bd5b5ee4ba36880
Akhil Kumar <akhilguretia@gmail.com>
2024-08-08 22:01:35 +0530

    Section 2

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -1,12 +1,14 @@
 import Layout from "../../components/Layouts/Layout";
 import Section1 from "./Section1";
 import "../../styles/HomeStyles.css";
+import Section2 from "./Section2";
 
 function Home() {
   return (
     <>
       <Layout>
         <Section1 />
+        <Section2 />
       </Layout>
     </>
   );
